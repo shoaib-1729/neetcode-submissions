e4799bb64c139f2f7f9ea5615d5f5450af56b0a5
Shoaib Akhtar <shoaibahktar56@gmail.com>
2026-07-15 23:27:46 +0530

    Add: majority-element-ii - submission-3

---
+++ Data Structures & Algorithms/majority-element-ii/submission-3.js
@@ -0,0 +1,61 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+    // kyuki ans worst case mei bss 2 hi elements store karega...
+    majorityElement(nums) {
+
+        let candidate1 = nums[0];
+        let candidate2 = nums[0];
+        let vote1 = 0;
+        let vote2 = 0;
+        
+        for(let i=0; i<nums.length; i++){
+            if(vote1 == 0 && candidate2 != nums[i]){
+                candidate1 = nums[i]
+                vote1 = 1
+            }
+
+            if(vote2 == 0 && candidate1 != nums[i]){
+                candidate2 = nums[i]
+                vote2 = 1 
+            }
+
+            else if(nums[i] == candidate1)
+                vote1++
+
+            else if(nums[i] == candidate2) vote2++
+
+          
+
+            else{
+                vote1--
+                vote2--
+            }
+        }
+
+
+         // verify ki sach mei zyada hai ki nahi
+        vote1 = 0
+        vote2 = 0
+
+        let ans = []
+
+        for(let i=0; i<nums.length; i++){
+            if(candidate1 == nums[i]) vote1++
+            else if(candidate2 == nums[i]) vote2++
+         }
+
+        let count = Math.floor(nums.length / 3)
+
+        if(vote1 > count) ans.push(candidate1)
+        if(vote2 > count) ans.push(candidate2)
+
+        return ans
+
+
+    }
+}
