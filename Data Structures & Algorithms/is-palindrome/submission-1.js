5a85f738bafccf297624bafd843e33ce3240f5ff
Shoaib Akhtar <shoaibahktar56@gmail.com>
2026-07-17 20:55:34 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+/*    
+    Analysis:
+        Time Complexity  : O(n)
+        Space Complexity : O(1)
+*/
+    
+
+    isPalindrome(s) {
+        // do pointers lelo, ismei mirror image wala concept lagega..
+        let start = 0;
+        let end = s.length - 1;
+
+        let isPalindrome = true;
+
+        while(start < end){
+            // agar start ka character alphanumeric nahi hai, toh usko skip karke aage badh jaao
+            if(!(/[a-zA-Z0-9]/).test(s[start])){
+                start++
+                continue
+            }
+
+            // agar end ka character alphanumeric nahi hai, toh usko skip karke aage badh jaao
+            if(!(/[a-zA-Z0-9]/).test(s[end])){
+                end--
+                continue
+            }
+
+            // agar hai toh
+            if(s[start].toLowerCase() != s[end].toLowerCase()) isPalindrome = false;
+
+            start++;
+            end--;
+        }
+
+        return isPalindrome;
+    }
+}
