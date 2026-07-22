dd5990d74eb7cf39030bfd7683f5b9edef1b2130
Shoaib Akhtar <shoaibahktar56@gmail.com>
2026-07-22 21:58:30 +0530

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        let left = 0;
+        let right = numbers.length - 1;
+
+        while(left < right){
+            // sum nikaalo dono elements ka
+             let sum = numbers[left] + numbers[right];
+
+            //  agar sum barabar hai target element se
+            if(sum === target){
+                return [left + 1, right + 1]
+            }
+
+            // agar sum chhota hai target se
+            else if(sum < target){
+                // sum ko badhao..
+                left++
+            }
+
+            // agar sum bada hai target se..
+            else if(sum > target){
+                // sum ko chhota karo..
+                right--;
+            }
+        }
+
+    }
+}
